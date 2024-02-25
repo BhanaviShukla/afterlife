e5a2cebbe4e4b60fd467b591539e5a4486365b0b
BhanaviShukla <bhanvi@shuklapaksha.com>
2024-02-25 15:35:28 +0400

    chore(css-cleanup): passing empty string instead of undefined as classname

--- src/components/ui/Typography/typography.jsx
+++ src/components/ui/Typography/typography.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Typography = ({
-    variant = 'paragraph',
-    className, 
-    children,
-}) => {
-    const component = (() => {
-        switch (variant) {
-        case 'title':
-        case 'h1':
-            return <h1 className={`title ${className}`}>{children}</h1>
-        case 'title-small':
-        case 'h2': 
-            return <h2 className={`title-small ${className}`}>{children}</h2>
-        case 'subtitle':
-        case 'h4': 
-            return <h4 className={`subtitle ${className}`}>{children}</h4>
-        case 'heading':
-        case 'h6': 
-            return <h6 className={`heading ${className}`}>{children}</h6>
-        case 'caption':
-            return <span className={`caption ${className}`}>{children}</span>
-        case 'paragraph':
-        default:
-            return <p className={`${className}`}>{children}</p>
-    }})()
-    return component 
-}
+const Typography = ({ variant = "paragraph", className, children }) => {
+  const customClassname = className || "";
+  const component = (() => {
+    switch (variant) {
+      case "title":
+      case "h1":
+        return <h1 className={`title ${customClassname}`}>{children}</h1>;
+      case "title-small":
+      case "h2":
+        return <h2 className={`title-small ${customClassname}`}>{children}</h2>;
+      case "subtitle":
+      case "h4":
+        return <h4 className={`subtitle ${customClassname}`}>{children}</h4>;
+      case "heading":
+      case "h6":
+        return <h6 className={`heading ${customClassname}`}>{children}</h6>;
+      case "caption":
+        return <span className={`caption ${customClassname}`}>{children}</span>;
+      case "paragraph":
+      default:
+        return <p className={`${customClassname}`}>{children}</p>;
+    }
+  })();
+  return component;
+};
 
-export default Typography
+export default Typography;
