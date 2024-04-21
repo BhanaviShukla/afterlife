4e0a91e57d7dddffb5176a33a393eed55bc739b1
BhanaviShukla <bhanvi@shuklapaksha.com>
2024-04-21 12:37:17 +0400

    fix(code): use variable over hardcoded value

--- src/components/ui/CarouselIndicator/CarouselIndicator.jsx
+++ src/components/ui/CarouselIndicator/CarouselIndicator.jsx
@@ -3,11 +3,15 @@ const CarouselIndicator = ({ itemCount, activeItemIndex }) => {
   const indicatorArray = [...Array(itemCount)].map((_, index) => (
     <EllipseIcon
       key={index}
-      color={index === activeItemIndex ? "#285857" : "#EDEDED"}
+      style={{
+        color:
+          index === activeItemIndex
+            ? "var(--colour-g300)"
+            : "var(--colour-n50)",
+      }}
       width={24}
     />
   ));
-  console.log({ indicatorArray });
   return (
     <div className="flex align-middle justify-center mt-8">
       {indicatorArray}
