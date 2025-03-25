f9a489060d3be00d8292fa1e762dee440b99ad21
Hasmuddin <mdaliansari33@gmail.com>
2025-03-25 20:57:49 +0530

    beneficiary configured

--- src/views/Asset/AssetBeneficiaryView.jsx
+++ src/views/Asset/AssetBeneficiaryView.jsx
@@ -1,8 +1,11 @@
-import { Typography } from "@/components"
+import { useState } from "react"
+import { Button, Typography } from "@/components"
+import AddChildIcon from "@/components/ui/Icons/Controls/add-user.svg";
 import { BackSubmitBtn } from "@/components/ui/Button/BackSubmitBtn"
 import { useParams } from "next/navigation"
+import { Beneficiary } from "./Beneficiary";
 
-const replaceWith = (url, to)=> url.replace('{{asset}}', to)
+const replaceWith = (url, to) => url.replace('{{asset}}', to)
 
 export const AssetBeneficiaryView = ({  
   backLink,
@@ -15,17 +18,36 @@ export const AssetBeneficiaryView = ({
   }) => {
   const { step } = useParams();
   const assetName = step.at(-2)
-  console.log('first', 
-    backLink,
-    secondaryCta,
-    primaryCta,
-    nextLink,
-    title,
-    assets,)
+
+  const [beneficiaries, setBeneficiaries] = useState([{ id: Date.now(), allocation: 0 }])
+
+  const onAddBeneficiary = () => {
+    setBeneficiaries((prevChildren) => [
+      ...prevChildren,
+      {
+        id: Date.now(),
+        allocation: 0,
+      },
+    ]);
+  };
+
   return (
         <section>
-           <Typography variant="title-small" className={'my-10'}>{replaceWith(assets?.title, assetName)}</Typography>
-           <BackSubmitBtn 
+          <Typography variant="title-small" className={'my-10'}>{replaceWith(assets?.title, assetName)}</Typography>
+          <form>
+            {beneficiaries.map((beneficiary)=> (
+                <Beneficiary key={beneficiary.id} {...beneficiary} />
+            ))}
+          </form>
+            <Button
+              variant="text"
+              leftIcon={<AddChildIcon />}
+              className="mt-12"
+              onClick={onAddBeneficiary}
+            >
+              Add another beneficiary
+            </Button>
+          <BackSubmitBtn 
               {...{
                   secondaryCta,
                   primaryCta,
