c9475a1495e372e46b277d516a3f0642fd0265da
Hasmuddin <mdaliansari33@gmail.com>
2025-05-03 18:11:13 +0530

    Added inline style

--- src/app/journey/layout.js
+++ src/app/journey/layout.js
@@ -1,10 +1,9 @@
 import { Logo } from "@/components";
-import style from './layout.module.css'
 
 export default function JourneyLayout({ children }) {
   return (
     <div className="container min-h-screen flex flex-col justify-between gap-6 py-10">
-      <a className={style.logoSpace} href="/">
+      <a className={`pt-[5px] pb-[5px] pl-[7px] pr-[7px]`} href="/">
         <Logo width={80} height={20} className={'mx-2'} />
       </a>
       <section className="flex flex-col justify-around grow" id="journey-view">
