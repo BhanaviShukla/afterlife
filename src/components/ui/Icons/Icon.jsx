77a3d947239d7c4388c5355bbdd87f5da5d7ac16
Hasmuddin <mdaliansari33@gmail.com>
2025-05-03 18:27:58 +0530

    Update Icon.jsx

--- src/components/ui/Icons/Icon.jsx
+++ src/components/ui/Icons/Icon.jsx
@@ -2,40 +2,25 @@ import React from "react";
 // import styles from "./iconStyles.module.css";
 import Image from "next/image";
 
-import DesignNib from './Informational/Landing/design-nib.svg'
-import User from './Informational/Landing/user.svg'
-import DocSearch from './Informational/Landing/doc-search-alt.svg'
-import EmojiSatisfied from './Informational/Landing/emoji-satisfied.svg'
-import Group from './Informational/Landing/group.svg'
-import MoneySquare from './Informational/Landing/money-square.svg'
-import TextSize from './Informational/Landing/text-size.svg'
-import Timer from './Informational/Landing/timer.svg'
-import Heart from './Informational/Landing/heart.svg'
-import Scale from './Informational/Landing/scale.svg'
-import Brain from './Informational/Landing/brain.svg'
-import Home from "./Informational/home.svg";
-import Car from "./Informational/car.svg"
-import Cash from "./Informational/money.svg";
-import Gift from "./Informational/gift.svg";
-
+import DesignNib from "./Informational/Landing/design-nib.svg";
+import User from "./Informational/Landing/user.svg";
+import DocSearch from "./Informational/Landing/doc-search-alt.svg";
+import EmojiSatisfied from "./Informational/Landing/emoji-satisfied.svg";
+import Group from "./Informational/Landing/group.svg";
+import MoneySquare from "./Informational/Landing/money-square.svg";
+import TextSize from "./Informational/Landing/text-size.svg";
+import Timer from "./Informational/Landing/timer.svg";
 
 const landingIcons = {
   "design-nib": DesignNib,
   user: User,
-  'doc-search-alt': DocSearch,
-  'emoji-satisfied': EmojiSatisfied,
-  'group': Group,
-  'money-square': MoneySquare,
-  'text-size': TextSize,
+  "doc-search-alt": DocSearch,
+  "emoji-satisfied": EmojiSatisfied,
+  group: Group,
+  "money-square": MoneySquare,
+  "text-size": TextSize,
   timer: Timer,
-  brain: Brain,
-  scale: Scale,
-  heart: Heart,
-  home: Home,
-  car: Car,
-  gift: Gift,
-  cash: Cash,
-}
+};
 
 const Icon = ({ name, size, color }) => {
   const IconComponent = landingIcons[name];
